f8ee43bea2ef10aafd545d9d62b245a2b5739306
DESKTOP-NB6D13P\Administrator <1014421907@qq.com>
2020-11-05 13:19:09 +0800

    vue

---
+++ src/views/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="index_upload">
+    <div class="title">成分检测上传图像</div>
+    <Upload />
+  </div>
+</template>
+
+<script>
+import Upload from "../components/index_upload/upload";
+export default {
+  name: "Index_upload",
+  components: {
+    Upload,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.index_upload{
+  height: 100%;
+  .title {
+  font-size: 60px;
+  margin:30px 0px ;
+}
+}
+
+</style>
